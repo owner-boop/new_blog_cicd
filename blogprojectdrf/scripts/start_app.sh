528aa5427b8573bdee83f5a3e70baa6404ca95a7
owner-boop <alihaider191919a@gmail.com>
2024-06-22 16:58:09 +0500

    Update start_app.sh

--- blogprojectdrf/scripts/start_app.sh
+++ blogprojectdrf/scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["Your_Instance_Public_IP"]/' /home/ubuntu/blogprojectdrf/blog/settings.py
+sed -i 's/\[]/\["3.87.22.51"]/' /home/ubuntu/blogprojectdrf/blog/settings.py
 
 python manage.py migrate 
 # python manage.py makemigrations     
