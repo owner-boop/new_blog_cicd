9a9fe5eca915dcb2df4911079518fd766e4bc63e
owner-boop <alihaider191919a@gmail.com>
2024-06-22 16:39:52 +0500

    Update nginx.sh

--- blogprojectdrf/scripts/nginx.sh
+++ blogprojectdrf/scripts/nginx.sh
@@ -8,14 +8,7 @@ sudo cp /home/ubuntu/blogprojectdrf/nginx/nginx.conf /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
-sudo fuser -k 80/tcp
-sudo fuser -k 443/tcp
-netstat -tnlp | grep -w 80
-netstat -ano|grep 80|grep LISTEN
-sudo netstat -lnp
-sudo lsof -i :80
-netstat -tulpn
-grep -rnw /etc/nginx/ -e '80'
+
 
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
