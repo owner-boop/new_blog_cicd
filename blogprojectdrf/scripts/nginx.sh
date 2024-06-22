e82b9fee2b7d48a32c40a14d9cbfce708c488828
owner-boop <alihaider191919a@gmail.com>
2024-06-22 15:47:21 +0500

    Update nginx.sh

--- blogprojectdrf/scripts/nginx.sh
+++ blogprojectdrf/scripts/nginx.sh
@@ -2,12 +2,12 @@
 #!/usr/bin/bash
 
 #sudo systemctl daemon-reload
-sudo rm -f /etc/nginx/sites-enabled/default
+#sudo rm -f /etc/nginx/sites-enabled/default
 
 sudo cp /home/ubuntu/blogprojectdrf/nginx/nginx.conf /etc/nginx/sites-available/fodler_name_where_seetngs.py_is
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
-sudo gpasswd -a www-data ubuntu
-sudo systemctl restart nginx
+#sudo gpasswd -a www-data ubuntu
+#sudo systemctl restart nginx
 
