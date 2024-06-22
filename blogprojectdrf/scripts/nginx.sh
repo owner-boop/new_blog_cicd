b6a47f4b9215b4d4ec125acd496ac87f7aebaefd
owner-boop <alihaider191919a@gmail.com>
2024-06-22 16:23:51 +0500

    Update nginx.sh

--- blogprojectdrf/scripts/nginx.sh
+++ blogprojectdrf/scripts/nginx.sh
@@ -11,5 +11,5 @@ sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
 sudo fuser -k 80/tcp
 sudo fuser -k 443/tcp
 sudo gpasswd -a www-data ubuntu
-sudo service restart nginx
+sudo systemctl restart nginx
 
